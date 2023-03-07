3125462eed01507cc7a0eacef810f6cddd2a3973
LocAdmin <zorkin1@yandex.ru>
2023-03-07 09:16:59 +0300

    тест

---
+++ files.py
@@ -0,0 +1,33 @@
+from os import path
+import openpyxl
+from openpyxl import load_workbook
+#def reading_files():
+#    full_name = path.basename(r'C:\tram\mgt_example_trans.csv ')
+#    name = path.splitext(full_name)
+#    print("прочитан файл", name)
+#   f = open('C://tram/mgt_example_trans.csv ', 'r')
+#   print(f.read())
+#   f.close()
+#book_scv = openpyexcel.open("mgt_example_trans.csv", read_only=True)
+
+
+book = openpyxl.open("C://tram/справочник.xlsx")
+sheet = book.active
+#print({sheet.iter_rows(min_row=1, max_col=3, max_row=2, values_only=True)}
+#col = 1
+#for i in range(1, 40):
+#    print(f' значение строке файла {i} колонки {col} равно значение  {sheet.cell(row=i,column=col).value}')
+
+print( {sheet.iter_rows(min_row=1, max_col=3, max_row=2, values_only=True)})
+#           {sheet.cell(row=i,column=col).value},
+##         {sheet.cell(row=i,column=col+2).value},
+#             {sheet.cell(row=i,column=col+3).value},
+#           {sheet.cell(row=i,column=col+4).value},
+#           {sheet.cell(row=i,column=col+5).value},
+#           {sheet.cell(row=i,column=col+6).value},
+#           {sheet.cell(row=i,column=col+7).value},
+#           {sheet.cell(row=i,column=col+8).value},
+#           {sheet.cell(row=i,column=col+9).value},
+#           {sheet.cell(row=i,column=col+10).value},
+#           {sheet.cell(row=i,column=col+10).value}
+#)
---
+++ find_PSG_DEVICE.py
@@ -0,0 +1,41 @@
+import openpyxl
+import csv
+
+find_vagon=0
+no_find_vagon=0
+no_data=0
+print('Поиск')
+with open('C://tram/mgt_trans_01032023.csv') as file:
+    reader = csv.DictReader(file, delimiter=";")
+    for row in reader:
+        row = str(row['PSG_DEVICE'])
+        row_find = row
+#        print(row_find)
+
+        book = openpyxl.open("C://tram/справочник.xlsx", read_only=True)
+        sheet = book.active
+        count = 0
+        validator = []
+        for i in sheet.values:
+            count += 1
+            i = str(i)
+            x = i.find(row_find)
+            if x > 0:
+
+#                print('Вагон', row, 'найден в строке', count)
+                validator.append(i)
+#                print(validator)
+                find_vagon+=1
+                print({find_vagon}, {no_find_vagon}, {no_data})
+
+            elif x ==-1:
+#               print(x, 'Вагон', row_find, 'не найден в строке', count)
+               no_find_vagon+=1
+            else:
+                pass
+                no_data+=1
+                print('Нулевое значение')
+
+print('Найдено', find_vagon)
+print('Не найдено',no_find_vagon)
+print('Нет данных',no_data)
