5333e38d2f1a906b12cbcb1a9712810499e8dd9d
LocAdmin <zorkin1@yandex.ru>
2023-03-16 16:59:52 +0300

    сделал пример на pandas сначала загружается справочник как DF далее извлекается валидация и находит возвращает  отдель DF c True из справочника Нужно условие что действительно найден валидатор т.е в DF есть скаладывать строку валидации + строку справочника и писать в файл

--- fu.py
+++ fu.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import pandas as pd
 #функция определения максимального количества строк в csv файле
 #пример
     #import fu
@@ -15,5 +16,14 @@ def max_line_file_csv(file):
         print('Максимальное количество строк в файле',p,'-', reader.line_num)
         i=reader.line_num
     return (i)
+def xlsx_to_csv_pd():
+#функция преобразования xls файла в scv
+    data_xls = pd.read_excel('C:/tram/справочник.xlsx', index_col=0)
+    data_xls.to_csv('C:/tram/справочник_xlsx_to_csv.csv', encoding='cp1251', sep=';')
+
+
+if __name__ == '__main__':
+    xlsx_to_csv_pd()
+
 
 #max_line_file_csv('mgt_example_trans.csv')
--- Тесты_Олег.py
+++ Тесты_Олег.py
@@ -1,36 +1,41 @@
 
-import fu
-from csv import writer
-import csv
 import pandas as pd
 
-# fu.max_line_file_csv('C:/tram/mgt_trans_01032023.csv')
-# max_line=fu.max_line_file_csv('mgt_example_trans.csv')
-# i = 4267343/500000
-# print (i)
+all_row = 0
+df = pd.read_excel('C:/tram/справочник.xlsx', index_col=0)
+#print(df)
+print('справочник прочитан')
+print('чтение строк выгрузки вылидаций')
+#df_data = pd.read_csv('C:/tram/mgt_trans_01032023.csv', sep=';')
 
-max_line_in_file = 10
-#with open('C:/tram/mgt_example_trans.csv') as file_csv:
-df = pd.read_csv('C:/tram/mgt_trans_01032023.csv', sep=';')
-all_row=df.index.stop+1
-print('В файле',all_row ,"строк")
+df_data = pd.read_csv('C:/tram/mgt_example_trans.csv', sep=';')
+start_index = df_data.index.start
+stop_index = df_data.index.stop
+x = 0 T
+while x < stop_index:
+    data_index = df_data.iloc[x]['PSG_DEVICE']
+#    print(x, 'строка - значение', data_index)
+    name_file=str('find_data')+str(x)
+    find_data = df.isin([data_index])
 
-#df.to_csv('C:/tram/new_file.csv', sep=';')
+#    find_data.to_csv('C:/tram/find_data', encoding='cp1251', sep=';')
+    print(find_data)
+    x+= 1
 
-#with open('C:/tram/mgt_example_trans.csv') as file_csv, open('C:/tram/new_file.csv', 'w') as f:
-#    reader = csv.reader(file_csv, delimiter=";")
-#    file_writer = csv.writer(f, delimiter=";", )
-#    for stroka in reader:
-#        pd.to_csv('C:/tram/new_file.csv')
-#        print(stroka)
-#        writer_object = writer(f, delimiter=";")
-#        print('writer_object', reader.line_num)
-#        writer_object.writerow(stroka)
-#        print('writer_object.writerow(row)', reader.line_num)
 
-#        writer = writer(f, delimiter=";")
- #       writer.csv.writerows(row)
-#       print()
-#        if reader.line_num == max_line_in_file: break
 print('программа завершена')
+#name_file='find_data'+1
 
+#    name_file = str('C:/tram/')+str('find_data') + str(x)
+#find_data.to_csv('C:/tram/log/find_data.csv', encoding='cp1251', sep=';')
+#     print_data=df_data.loc[[1], ['PSG_DEVICE']]
+#     print({print_data)
+#print('файл валидаций прочитан')
+#all_row=df.index.stop+1
+#print('В файле',all_row ,"строк")
+#df.to_csv('C:/tram/new_file.csv', sep=';')
+#with open('C:/tram/mgt_example_trans.csv') as file_csv, open('C:/tram/new_file.csv', 'w') as f:
+#print('В файле', all_row, "строк")
+#i=all_row
+#print(all_row)
+#print(df_data.index.stop+1)
