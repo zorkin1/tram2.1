76b5a76a3680b1a3e113786afcbd1d027060268c
LocAdmin <zorkin1@yandex.ru>
2023-03-17 15:34:02 +0300

    Начата обработка Fri Mar 17 13:33:26 2023 обработано строк 4267342 закончено Fri Mar 17 15:20:05 2023писать в файл результаты обработки

--- Тесты_Олег.py
+++ Тесты_Олег.py
@@ -1,29 +1,34 @@
 
 import pandas as pd
+import time
+
+import progressbar
+from progress.bar import IncrementalBar
 
 all_row = 0
 df = pd.read_excel('C:/tram/справочник.xlsx', index_col=0)
 #print(df)
 print('справочник прочитан')
 print('чтение строк выгрузки вылидаций')
-#df_data = pd.read_csv('C:/tram/mgt_trans_01032023.csv', sep=';')
-
-df_data = pd.read_csv('C:/tram/mgt_example_trans.csv', sep=';')
+df_data = pd.read_csv('C:/tram/mgt_trans_01032023.csv', sep=';')
+print('строк выгрузки прочитаны')
+#df_data = pd.read_csv('C:/tram/mgt_example_trans.csv', sep=';')
 start_index = df_data.index.start
 stop_index = df_data.index.stop
-x = 0 T
+start_time=time.ctime()
+#bar = progressbar.ProgressBar(maxval=stop_index)
+x = 0
 while x < stop_index:
     data_index = df_data.iloc[x]['PSG_DEVICE']
-#    print(x, 'строка - значение', data_index)
-    name_file=str('find_data')+str(x)
+#    name_file=str('find_data')+str(x)
     find_data = df.isin([data_index])
+    x += 1
+#    print( int(x / stop_index*100), "%")
 
-#    find_data.to_csv('C:/tram/find_data', encoding='cp1251', sep=';')
-    print(find_data)
-    x+= 1
-
-
+print('Начата обработка', start_time, 'обработано строк',x , 'закончено', time.ctime(),)
 print('программа завершена')
+#    find_data.to_csv('C:/tram/find_data', encoding='cp1251', sep=';')
+    #    print(find_data)
 #name_file='find_data'+1
 
 #    name_file = str('C:/tram/')+str('find_data') + str(x)
