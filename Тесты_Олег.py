9fee8775e3935908f1b6bed94cb5698e1007e9cf
LocAdmin <zorkin1@yandex.ru>
2023-03-23 16:59:13 +0300

    Начата обработка Fri Mar 17 13:33:26 2023 обработано строк 4267342 закончено Fri Mar 17 15:20:05 2023писать в файл результаты обработки

--- Тесты_Олег.py
+++ Тесты_Олег.py
@@ -10,23 +10,31 @@ df = pd.read_excel('C:/tram/справочник.xlsx', index_col=0)
 #print(df)
 print('справочник прочитан')
 print('чтение строк выгрузки вылидаций')
-df_data = pd.read_csv('C:/tram/mgt_trans_01032023.csv', sep=';')
+#df_data = pd.read_csv('C:/tram/mgt_trans_01032023.csv', sep=';')
 print('строк выгрузки прочитаны')
-#df_data = pd.read_csv('C:/tram/mgt_example_trans.csv', sep=';')
+df_data = pd.read_csv('C:/tram/mgt_example_trans.csv', sep=';')
 start_index = df_data.index.start
 stop_index = df_data.index.stop
 start_time=time.ctime()
+find_list_coloms =['тип вагона', 'депо', ' № вагона', '№№ валидаторов 1', '№№ валидаторов 2',   '№№ валидаторов 3', '№№ валидаторов 4', '№№ валидаторов 5', '№№ валидаторов 6', 'Примечание ']
 #bar = progressbar.ProgressBar(maxval=stop_index)
 x = 0
 while x < stop_index:
     data_index = df_data.iloc[x]['PSG_DEVICE']
-#    name_file=str('find_data')+str(x)
-    find_data = df.isin([data_index])
+    print(type(data_index))
+    #    name_file=str('find_data')+str(x)
+
+    find_data = (df.isin([data_index]))
+#    find_row = find_data[find_data[find_list_coloms] == 'True'].index
+    print(find_data)
+
     x += 1
 #    print( int(x / stop_index*100), "%")
 
-print('Начата обработка', start_time, 'обработано строк',x , 'закончено', time.ctime(),)
+
+#print('Начата обработка', start_time, 'обработано строк',x , 'закончено', time.ctime(),)
 print('программа завершена')
+
 #    find_data.to_csv('C:/tram/find_data', encoding='cp1251', sep=';')
     #    print(find_data)
 #name_file='find_data'+1
@@ -44,3 +52,15 @@ print('программа завершена')
 #i=all_row
 #print(all_row)
 #print(df_data.index.stop+1)
+
+#
+#вагона
+#депо     № вагона    №№ валидаторов
+#1    №№ валидаторов
+#2    №№ валидаторов
+#3    №№ валидаторов
+#4    №№ валидаторов
+#5    №№ валидаторов
+#6
+#Примечание
+find_list_coloms =['тип вагона', 'депо', ' № вагона', '№№ валидаторов 1', '№№ валидаторов 2',   '№№ валидаторов 3', '№№ валидаторов 4', '№№ валидаторов 5', '№№ валидаторов 6', 'Примечание ']
